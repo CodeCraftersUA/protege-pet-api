fc26a4bfaad002e5595bd08a96d243e07b5c177a
Luiz Rodrigues <luizhenriquealvesrodrigues64@gmail.com>
2023-03-29 19:39:08 -0300

    style: identation

--- middlewares/validations/index.js
+++ middlewares/validations/index.js
@@ -1,19 +1,19 @@
 import AppError from "../../errors/AppError.js";
 
 export const validate = schema => async (req, res, next) => {
-    try {
-        await schema.validate({
-            body: req.body,
-            query: req.query,
-            params: req.params
-        });
+  try {
+    await schema.validate({
+      body: req.body,
+      query: req.query,
+      params: req.params
+    });
 
-        return next();
-    } catch (err) {
-        if (err.path) {
-            throw new AppError(`${err.path} is invalid`);
-        } else {
-            throw err;
-        }
+    return next();
+  } catch (err) {
+    if (err.path) {
+      throw new AppError(`${err.path} is invalid`);
+    } else {
+      throw err;
     }
+  }
 };
