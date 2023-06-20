ffdc23e8383ab494be74ea2200ec7a7f6113f375
Luiz Rodrigues <luizhenriquealvesrodrigues64@gmail.com>
2023-06-20 20:59:56 -0300

    feat: transfer animal on accept transference

--- src/modules/transferences/useCases/UpdateTransference/UpdateTransferenceUseCase.ts
+++ src/modules/transferences/useCases/UpdateTransference/UpdateTransferenceUseCase.ts
@@ -14,16 +14,39 @@ interface params {
 class UpdateTransferenceUseCase {
   execute = async ({ id, status }: params): Promise<AnimalTransference> => {
     try {
-      const queryResult = await prisma.animalTransference.update({
-        data: {
-          status
-        },
-        where: {
-          id
-        }
-      });
+      if (status === "ACCEPTED") {
+        const transferenceData = await prisma.animalTransference.findFirstOrThrow({
+          where: { id }
+        })
 
-      return queryResult;
+        const queryResult = await prisma.$transaction([
+          prisma.animalTransference.update({
+            data: {
+              status
+            },
+            where: { id }
+          }),
+          prisma.animal.update({
+            data: {
+              owner: transferenceData.receiverId
+            },
+            where: {
+              id: transferenceData.animalId
+            }
+          })
+        ]);
+
+        return queryResult[0];
+      } else {
+        const queryResult = await prisma.animalTransference.update({
+          data: {
+            status
+          },
+          where: { id }
+        });
+
+        return queryResult;
+      }
     } catch (err) {
       if (err?.code === "P2025")
         throw new AppError(`Could not find id ${id}`, 404);
