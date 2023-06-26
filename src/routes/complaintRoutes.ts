c72df26968913903d57a4b5297005b8719682c9f
Luiz Rodrigues <luizhenriquealvesrodrigues64@gmail.com>
2023-06-26 20:15:21 -0300

    feat: implement list complaints route

--- src/routes/complaintRoutes.ts
+++ src/routes/complaintRoutes.ts
@@ -3,9 +3,10 @@ import express from "express";
 
 // Controllers
 import CreateComplaintController from "../modules/complaint/useCases/createComplaint/CreateComplaintController.js";
+import ListComplaintController from "../modules/complaint/useCases/listComplaint/ListComplaintController.js";
 
 // Middlewares
-//import Authenticate from "../middlewares/authenticate.js";
+import Authenticate from "../middlewares/authenticate.js";
 import { createComplaintValidate } from "../middlewares/validations/complaint/complaint.js";
 
 // Types
@@ -13,10 +14,13 @@ import { UserType } from "../models/account.js";
 
 const app = express();
 
+const authenticateProtector = new Authenticate(UserType.PROTECTOR);
+
 const createComplaintController = new CreateComplaintController();
+const listComplaintController = new ListComplaintController();
 
-//const authenticateAdmin = new Authenticate(UserType.ADMIN);
 
+app.get("", authenticateProtector.execute, listComplaintController.handler);
 app.post("", createComplaintValidate, createComplaintController.handler);
 
 export default app;
